264919fd306e02c81540c767c9b076df3f0b467a
WesThorburn <wjthorburn@live.com.au>
2016-06-30 22:18:59 -0500

    Add basic multi-client functionality

--- app.js
+++ app.js
@@ -18,21 +18,30 @@ io.sockets.on('connection', function(socket){
 	socket.id = Math.random();
 	socket.x = 0;
 	socket.y = 0;
+	socket.number = "" + Math.floor(10 * Math.random());
 	SOCKET_LIST[socket.id] = socket;
 
-	socket.emit('serverMsg', {
-		msg: 'Hello'
+	socket.on('disconnect', function(){
+		delete SOCKET_LIST[socket.id];
 	});
 });
 
 setInterval(function(){
+	var pack = [];
+
 	for(var i in SOCKET_LIST){
 		var socket = SOCKET_LIST[i];
 		socket.x++;
 		socket.y++;
-		socket.emit('newPosition', {
+		pack.push({
 			x:socket.x,
-			y:socket.y
-		});
+			y:socket.y,
+			number:socket.number
+		})
+	}
+
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions', pack);
 	}
 }, 1000/25)
