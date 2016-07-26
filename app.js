79f12c02d1f2c2172868db4f4512a7701f28a4ec
WesThorburn <wjthorburn@live.com.au>
2016-07-26 14:22:37 -0500

    Fix collision detection bug
    Addition was used instead of subtracting resulting in an incorrect collision box rotation angle

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ var Entity = function(param){
 
 		//var collisionFactor = Math.pow(self.x - collisionCenterX, 2)/Math.pow(playerCollisionRadiusX, 2) + Math.pow(self.y - collisionCenterY, 2)/Math.pow(playerCollisionRadiusY, 2);
 
-		var collisionFactor = Math.pow(Math.cos(mouseAngleRadians) * (self.x - collisionCenterX) + Math.sin(mouseAngleRadians) * (self.y - collisionCenterY), 2) / Math.pow(playerCollisionRadiusX, 2) + Math.pow(Math.sin(mouseAngleRadians) * (self.x - collisionCenterX) + Math.cos(mouseAngleRadians) * (self.y - collisionCenterY), 2) / Math.pow(playerCollisionRadiusY, 2);
+		var collisionFactor = Math.pow(Math.cos(mouseAngleRadians) * (self.x - collisionCenterX) + Math.sin(mouseAngleRadians) * (self.y - collisionCenterY), 2) / Math.pow(playerCollisionRadiusX, 2) + Math.pow(Math.sin(mouseAngleRadians) * (self.x - collisionCenterX) - Math.cos(mouseAngleRadians) * (self.y - collisionCenterY), 2) / Math.pow(playerCollisionRadiusY, 2);
 
 		if(collisionFactor <= 1){
 			return true;
