c2de9644eefcfc8ab3ada0f6213143d5a4bca291
WesThorburn <wjthorburn@live.com.au>
2016-08-30 23:00:35 +1000

    Add title comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
     <title>Socket Game</title>
     <!-- Singapore Server 128.199.163.33 -->
+    <!-- nodejs -->
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
