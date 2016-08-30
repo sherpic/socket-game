ae31364f442110702cfd12b43ef52b9cc16f5ad7
WesThorburn <wjthorburn@live.com.au>
2016-08-30 22:28:26 +1000

    Add Server Name and Ip Address to HTML page comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Socket Game - Singapore</title>
+    <title>Socket Game</title>
+    <!-- Singapore Server 128.199.163.33 -->
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
